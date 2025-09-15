e9e8ec0fbaa7e370cb8c8e08df6e02c882edbe69
Patrick Mooney <github.pmooney78@neverbox.com>
2025-09-15 16:13:39 -0500

    Adds solutions for JavaScript exercises #6 - repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    if (count < 0) {
+        return 'ERROR'
+    }
+    let res = ''
+    for (let c = 1; c <= count; c++) {
+        res += str
+    }
+    return res
 };
 
 // Do not edit below this line
