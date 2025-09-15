90cc753a3da60757c192bf26e0436b0d088a449c
Patrick Mooney <github.pmooney78@neverbox.com>
2025-09-15 16:26:01 -0500

    Adds solutions for JavaScript exercises #7 - reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const ARR = str.split('')
+    let ret = []
+    for (let char of ARR) {
+        ret.unshift(char)
+    }
+    return ret.join('')
 };
 
 // Do not edit below this line
