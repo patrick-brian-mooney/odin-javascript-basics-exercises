3cae9fbefbaf38311c0e05e2994596d9fabb98eb
Patrick Mooney <github.pmooney78@neverbox.com>
2025-09-15 19:02:08 -0500

    Adds solutions for JavaScript exercises #9 - sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    if (typeof first != 'number' || typeof last != 'number') {
+        return "ERROR"
+    }
 
+    if (first < 0 || last < 0) {
+        return "ERROR"
+    }
+
+    if (!Number.isInteger(first) || !Number.isInteger(last)) {
+        return 'ERROR'
+    }
+
+    if (first > last) {
+        temp = first;
+        first = last;
+        last = temp;
+    }
+
+    let sum = 0;
+
+    for (let cur = first; cur <= last; cur++) {
+        sum += cur
+    };
+    return sum
 };
 
 // Do not edit below this line
