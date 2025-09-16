ae7c948ca20fed91bcdf4d1c7f83627afe74e865
Patrick Mooney <github.pmooney78@neverbox.com>
2025-09-15 19:15:37 -0500

    Adds solutions for JavaScript exercises #11 - tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const doRound = function(what) {      // round a number to one decimal place.
+  return Math.round(what * 10) / 10
+}
+
+const convertToCelsius = function(fahr) {
+  return doRound((fahr - 32) * (5 / 9))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  return doRound(32 + (cels * (9 / 5)))
 };
 
 // Do not edit below this line
